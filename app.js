4d013327b50d5c428056d4e164b91ec2cb9248d8
HASAN SAKALLI <89208116+hasansakalli@users.noreply.github.com>
2022-04-22 17:34:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,24 @@
-
-
-const body = document.querySelector("body");
-const dayNumberResult= document.getElementById("dayNumber-Result");
+const dayNumberResult = document.getElementById("dayNumber-Result");
 const hafta1 = document.querySelectorAll("#hafta1");
-dayNameResult= document.getElementById("dayName-Result");
-const sat= document.querySelectorAll(".sat");
-const fri= document.querySelectorAll(".fri");
-const inputText= document.getElementById("inputText");
-const todoResult= document.getElementById("todoResult");
-const todo= document.querySelector(".todo");
+dayNameResult = document.getElementById("dayName-Result");
+const inputText = document.getElementById("inputText");
+const todo = document.querySelector(".todo");
 
+hafta1.forEach((number) => {
+  number.addEventListener("click", showNumber);
 
+  function showNumber() {
+    dayNumberResult.innerHTML = this.textContent;
+    dayNameResult.innerHTML = this.className;
+    dayNameResult.style.console.log(this.className);
+  }
+});
 
-// let i=0
-// for(i=0; i< 31; i++){
-// pasiv[i].textContent;
-    
-// }
-// //  console.log(pasiv[i].textContent) 
+function addTodo() {
+  if (inputText.value !== "") var newP = document.createElement("li");
+  todo.appendChild(newP);
+  newP.classList.add(".todoResult");
+  newP.innerHTML = inputText.value;
 
-hafta1.forEach(number=> {
-    number.addEventListener("click", showNumber)
-       
-    function showNumber(){
-        dayNumberResult.innerHTML=this.textContent
-        dayNameResult.innerHTML=this.className
-        dayNameResult.style.
-        console.log(this.className)
-      
-      
-        }
-   
-});  
-
-function addTodo(){
-    if(inputText.value!=="")
-   var newP= document.createElement("li");
-    todo.appendChild(newP)
-    newP.classList.add(".todoResult")
-    newP.innerHTML= inputText.value;
-
-    inputText.value="";
-
+  inputText.value = "";
 }
